1f52d9d23079511582b96957f7fcbd0a6930441d
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-14 14:42:05 +0000

    added the landing and about to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-// import About from "@/Components/About";
-// import Landing from "@/Components/Landing";
+import About from "@/components/About";
+import Landing from "@/components/Landing";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="max-w-[65rem] mx-auto">
       <Navbar />
+      <Landing />
+      <About />
     </main>
   );
 }
